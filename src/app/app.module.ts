6e6df30f71e4fa638e1c7f0c8c9f2a6c4d56b3d8
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-06-21 13:53:27 +0300

    Добавил импорт httpclient и модуль добавления сотрудников

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeesListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, EmployeesListComponent, AddEmployeeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
