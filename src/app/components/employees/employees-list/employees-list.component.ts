707fe0c99886885a63a11d8abc18553ddf46a034
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-06-21 13:51:50 +0300

    Связал сервис и компонент списка сотрудников

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
+import { employees as data } from 'src/app/data/employees';
+import { EmployeesService } from 'src/app/services/employees.service';
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
-  styleUrls: ['./employees-list.component.css']
+  styleUrls: ['./employees-list.component.css'],
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
+  employees: Employee[] = [];
 
+  constructor(private employeesService: EmployeesService) {}
+
+  ngOnInit(): void {
+    this.employeesService
+      .getAllEmployees()
+      .subscribe((employees: Employee[]) => {
+        this.employees = employees;
+      });
+  }
 }
