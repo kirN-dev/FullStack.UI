b2bea680e0401e11164a87b025dd6dd2422e841d
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-06-21 13:51:05 +0300

    Добавил статические данные сотрудников для теста

---
+++ src/app/data/employees.ts
@@ -0,0 +1,28 @@
+import { Employee } from '../models/employee.model';
+
+export const employees: Employee[] = [
+  {
+    id: 'eda2a787-5096-4011-b10b-5653824ed03a',
+    name: 'Кирилл',
+    email: 'kirill@email.com',
+    phone: 89101234578,
+    salary: 60000,
+    department: 'СУП',
+  },
+  {
+    id: '625792b2-26bd-40f9-a5d3-35896a69ecc8',
+    name: 'Дима',
+    email: 'dima@email.com',
+    phone: 89101234545,
+    salary: 60000,
+    department: 'Отдел разработки',
+  },
+  {
+    id: '94f15476-5293-424c-9407-ebbdc906f296',
+    name: 'Даниил',
+    email: 'daniil@email.com',
+    phone: 89101234537,
+    salary: 60000,
+    department: 'Менеджмент',
+  },
+];
