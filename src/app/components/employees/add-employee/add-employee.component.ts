4abfaaf927c7f7e010343b6a62a6b288e18e09aa
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-06-21 15:21:54 +0300

    Подключил сервис по созданию сотрудника

--- src/app/components/employees/add-employee/add-employee.component.ts
+++ src/app/components/employees/add-employee/add-employee.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
-export class AddEmployeeComponent {
+export class AddEmployeeComponent implements OnInit {
+  addEmployeeRequest: Employee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: 0,
+    salary: 0,
+    department: '',
+  };
 
+  constructor(
+    private employeeService: EmployeesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  addEmployee(): void {
+    this.employeeService
+      .addEmployee(this.addEmployeeRequest)
+      .subscribe((employee) => {
+        this.router.navigate(['employees']);
+      });
+  }
 }
