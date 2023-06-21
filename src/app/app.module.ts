04d1eaac4c9a244831db75e03fd7b85d98491fa9
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-06-21 14:25:34 +0300

    Добавил модуль формы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, EmployeesListComponent, AddEmployeeComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
